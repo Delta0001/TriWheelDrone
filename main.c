cd6f97422640bddbce7595278f83ada8d50f45af
David <davidjoshuasim@gmail.com>
2016-12-29 22:40:30 -0800

    implementing pwm - not working yet

--- main.c
+++ main.c
@@ -1,32 +1,52 @@
 #include <reg51.h>
 
-sbit mybit=P1^0;
-
 void delay();
+void init();
 
-void main(void) {
-	TMOD = 0x01; // Timer 0 Gate:0 C/T:0 Mode:01 (16-bit timer)
+#define pwm_width P3;
+bit pwm_flag = 0;
+
+void main() {
+	init();
 	
-	P1 = 0x01;
 	while (1) {
 		delay();
-		delay();
-		delay();
-		delay();
-		P1 = P1<<1;
-		if (P1==0x00) P1 = 0x01;
+		//delay();
+		//delay();
+		//delay();
+		//P1 = P1<<1;
+		//if (P1==0x00) P1 = 0x01;
 	}
 }
 
-void delay(void) { // 25ms delay 
+void init() {
+	TMOD = 0x01;	// Timer0 Gate:0 C/T:0 Mode:01 (16-bit timer)
+	EA = 1; 		// Enable Interrupts
+	ET1 = 1;		// Enable interrupt timer1 
+	TR1 = 1; 		// Turn on timer0
+	P1 = 0x01;
+}
+void delay() { // 25ms delay 
 				// (12 crystal cycle = 1 machine cycle)
 				// (12/11.0599MHz)(2^16 - x)=25ms
 				// x = 2^16 - 25ms*11.0592MHz/12 = 42496 = A600H
 	TL0=0x00;	// load counting value
 	TH0=0xA6;	// load counting value 
-	TR0=1;		// turn on timer 0
+	TR0=1;		// turn on timer0
 	while (TF0==0); // wait for overflow flag
-	TR0=0;		// turn off timer 0 on overflow
+	TR0=0;		// turn off timer0 on overflow
 	TF0=0;		// clear overflow flag (won't be reset by hardware)
-	
+}
+
+void timer1() interrupt 3 { // implentation of http://www.8051projects.net/wiki/Pulse_Width_Modulation
+	if (!pwm_flag) {
+		pwm_flag = 1;
+		P1 = 0x01;
+		TH0 = pwm_width;
+	} else {
+		pwm_flag = 0;
+		P1 = 0x00;
+		TH0 = 255 - pwm_width;
+	}
+	TF0 = 0;	// clear overflow flag
 }
